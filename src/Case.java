b94cdea8e720cd964e0d40055dfce1666a2dc1c4
Stephanethr <stethiry9@gmail.com>
2024-02-06 14:43:53 +0100

    itération 1

--- src/Case.java
+++ src/Case.java
@@ -1,15 +1,9 @@
 public class Case {
-    private int position;
-    private String nom;
-    private String type;
-
-    
+    private int position;    
     
     // Constructeur
     public Case(int position, String nom, String type) {
         this.position = position;
-        this.nom = nom;
-        this.type = type;
     }
     
     // Méthodes pour accéder et modifier les attributs (getters/setters)
@@ -20,21 +14,5 @@ public class Case {
 
     public void setPosition(int position) {
         this.position = position;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
     }
 }
--- src/Game.java
+++ src/Game.java
@@ -6,35 +6,69 @@ import java.util.ArrayList;
 public class Game {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
 
-        while (true){
-            System.out.println("Bienvenue dans le jeu Monopoly");
-            System.out.println("Quel est votre pseudo ?");
+        System.out.println("Bienvenue dans le jeu Monopoly");
+        System.out.println("Voulez-vous commencer une partie ? (y/n)");
+        String reponse = System.console().readLine();
+        if (reponse.equals("n")) {
+            System.out.println("Au revoir !");
+            System.exit(0);
+        }
+
+        // Création des joueurs
+        System.out.println("Combien de joueurs ?");
+        int nbJoueurs = Integer.parseInt(System.console().readLine());
+        ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
+        for (int i = 0; i < nbJoueurs; i++) {
+            System.out.println("Quel est le pseudo du joueur " + (i + 1) + " ?");
             String pseudo = System.console().readLine();
-            // Création du joueur
             Joueur joueur = new Joueur(pseudo);
+            listeJoueur.add(joueur);
             System.out.println("Bienvenue " + joueur.getPseudo());
-            System.out.println("Voulez-vous commencer une partie ? (y/n)");
-            String reponse = System.console().readLine();
-            if (reponse.equals("n")){
-                break;
-            }
-            // Création du dé
-            De de1 = new De();
-            De de2 = new De();
+        }
 
-            // Création du plateau
-            ArrayList<Case> plateau = new ArrayList<Case>();
-            plateau.add(new Case(0, "Départ", "départ"));
-            while (plateau.size() < 40){
-                plateau.add(new Case(plateau.size(), "Case " + plateau.size(), "normal"));
-            }
-            System.out.println(plateau);
+        // Création des dés
+        De de1 = new De();
+        De de2 = new De();
 
+        // Création du plateau
+        ArrayList<Case> plateau = new ArrayList<Case>();
+        plateau.add(new Case(0, "Départ", "départ"));
+        while (plateau.size() < 40) {
+            plateau.add(new Case(plateau.size(), "Case " + plateau.size(), "normal"));
+        }
+
+        while (true) {
+            for (Joueur joueur : listeJoueur) {
+                System.out.println("------------------ TOUR DE " + joueur.getPseudo() + " ---------------------");
+                System.out.println("Appuyez sur entrée pour lancer le dé");
+                System.console().readLine();
+                de1.lancer();
+                de2.lancer();
+                System.out.println("valeur des dé : " + de1.getValue() + " et " + de2.getValue());
+
+                int deValue = de1.getValue() + de2.getValue();
+                System.out.println(joueur.getPseudo() + " a fait " + deValue);
+                joueur.deplacement(deValue);
+                System.out.println(joueur.getPseudo() + " est sur la case " + joueur.getPosition() + "\n");
+
+                // Si le joueur fait un double, il rejoue
+                // To Do: gérer le cas où le joueur va en prison s'il fait 3 doubles de suite
+                while ((de1.getValue() == 6) && (de1.getValue() == de2.getValue())) {
+                    System.out.println("Vous avez fait un double !");
+                    System.out.println("Vous pouvez rejouer");
+                    System.console().readLine();
+                    de1.lancer();
+                    de2.lancer();
+                    System.out.println("valeur des dé : " + de1.getValue() + " et " + de2.getValue());
+                    deValue = de1.getValue() + de2.getValue();
+                    System.out.println(joueur.getPseudo() + " a fait " + deValue);
+                    joueur.deplacement(deValue);
+                    System.out.println(joueur.getPseudo() + " est sur la case " + joueur.getPosition() + "\n");
+                }
+            }
 
         }
 
-    
     }
 }
