0f751114d5d3718a8750a27b87d4d7660c7721bf
Stephanethr <stethiry9@gmail.com>
2024-02-06 16:13:13 +0100

    Ajout d'un système de sélection de nombres de tours à jouer

--- src/Game.java
+++ src/Game.java
@@ -8,16 +8,34 @@ public class Game {
     public static void main(String[] args) {
 
         System.out.println("Bienvenue dans le jeu Monopoly");
-        System.out.println("Voulez-vous commencer une partie ? (y/n)");
-        String reponse = System.console().readLine();
-        if (reponse.equals("n")) {
-            System.out.println("Au revoir !");
-            System.exit(0);
+        System.out.println("Combiens de tours voulez-vous jouer ? (entrez un nombre)");
+        int nbTours = 0;
+        int compteurTours = 1;
+        try {
+            nbTours = Integer.parseInt(System.console().readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Vous devez entrer un nombre");
+            return;
+        }
+
+        if (nbTours < 1) {
+            System.out.println("Le nombre de tours doit être supérieur à 0");
+            return;
         }
 
         // Création des joueurs
         System.out.println("Combien de joueurs ?");
-        int nbJoueurs = Integer.parseInt(System.console().readLine());
+        int nbJoueurs = 0;
+        try{
+            nbJoueurs = Integer.parseInt(System.console().readLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Vous devez entrer un nombre");
+            return;
+        }
+        if (nbJoueurs < 2) {
+            System.out.println("Il faut au moins 2 joueurs pour jouer");
+            return;
+        }
         ArrayList<Joueur> listeJoueur = new ArrayList<Joueur>();
         for (int i = 0; i < nbJoueurs; i++) {
             System.out.println("Quel est le pseudo du joueur " + (i + 1) + " ?");
@@ -40,7 +58,7 @@ public class Game {
 
         while (true) {
             for (Joueur joueur : listeJoueur) {
-                System.out.println("------------------ TOUR DE " + joueur.getPseudo() + " ---------------------");
+                System.out.println("------------------ TOUR "+ compteurTours + " DE " + joueur.getPseudo() + " ---------------------");
                 System.out.println("Appuyez sur entrée pour lancer le dé");
                 System.console().readLine();
                 de1.lancer();
@@ -68,6 +86,11 @@ public class Game {
                     caseCourante = plateau.get(joueur.getPosition());
                     System.out.println(joueur.getPseudo() + " est sur la case " + caseCourante + "\n");
                 }
+            }
+            compteurTours++;
+            if (compteurTours-1 == nbTours) {
+                System.out.println("Fin de la partie");
+                return;
             }
 
         }
--- src/Joueur.java
+++ src/Joueur.java
@@ -1,11 +1,15 @@
 public class Joueur {
     private String pseudo;
     private int position;
+    private int nbTours;
 
     public Joueur(String pseudo) {
         this.pseudo = pseudo;
         this.position = 0;
+        this.nbTours = 0;
     }
+
+    // Méthodes pour accéder et modifier les attributs (getters/setters)
 
     public String getPseudo() {
         return pseudo;
@@ -18,6 +22,16 @@ public class Joueur {
     public void setPosition(int position) {
         this.position = position;
     }
+
+    public int getNbTours() {
+        return nbTours;
+    }
+
+    public void setNbTours(int nbTours) {
+        this.nbTours = nbTours;
+    }
+
+    // Autres méthodes
 
     public void deplacement(int deValue) {
         if (this.position + deValue >= 40) {
