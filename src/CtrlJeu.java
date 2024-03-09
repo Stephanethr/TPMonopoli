5afcf96dfa38d85ffb18c9acf05fcbc423f89476
Stephanethr <stethiry9@gmail.com>
2024-03-09 13:22:36 +0100

    update controller and ihm

--- src/CtrlJeu.java
+++ src/CtrlJeu.java
@@ -1,51 +1,49 @@
+import java.util.ArrayList;
+
 public class CtrlJeu {
     private Game game;
+    private int nbTours;
+    private int nbJoueurs;
 
-    public void jouer() {
-        int nbTours = 0;
-        int nbJoueurs = 0;
+    public CtrlJeu(int nbTours, int nbJoueurs) {
+        this.nbTours = nbTours;
+        this.nbJoueurs = nbJoueurs;
+    }
 
-        System.out.println("Bienvenue dans le jeu Monopoly");
-        System.out.println("Combiens de tours voulez-vous jouer ? (entrez un nombre)");
-        nbTours = Integer.parseInt(System.console().readLine());
-        System.out.println("Combien de joueurs ?");
-        nbJoueurs = Integer.parseInt(System.console().readLine());
+    public void createGame() {
         game = new Game(nbTours, nbJoueurs);
-        while(true)
-        {
-            for (Joueur joueur : game.getListeJoueur()) {
-                System.out.println("------------------ TOUR " + game.getCompteurTours() + " DE " + joueur.getPseudo()
-                        + " ---------------------");
-                System.out.println("Appuyez sur entrée pour lancer le dé");
-                System.console().readLine();
-                System.out.println("valeur des dé : " + game.getDes()[0].getValue() + " et " + game.getDes()[1].getValue());
 
-                int deValue = game.getDes()[0].getValue() + game.getDes()[1].getValue();
-                System.out.println(joueur.getPseudo() + " a fait " + deValue);
-                joueur.deplacement(deValue);
-                Case caseCourante = game.getPlateau().get(joueur.getPosition());
-                System.out.println(joueur.getPseudo() + " est sur la case " + caseCourante + "\n");
+    }
 
-                // Si le joueur fait un double, il rejoue
-                // To Do: gérer le cas où le joueur va en prison s'il fait 3 doubles de suite
-                while (game.getDes()[0].getValue() == game.getDes()[1].getValue()) {
-                    System.out.println("Vous avez fait un double !");
-                    System.out.println("Vous pouvez rejouer");
-                    System.console().readLine();
-                    System.out.println("valeur des dé : " + game.getDes()[0].getValue() + " et " + game.getDes()[1].getValue());
-                    deValue = game.getDes()[0].getValue() + game.getDes()[1].getValue();
-                    System.out.println(joueur.getPseudo() + " a fait " + deValue);
-                    joueur.deplacement(deValue);
-                    caseCourante = game.getPlateau().get(joueur.getPosition());
-                    System.out.println(joueur.getPseudo() + " est sur la case " + caseCourante + "\n");
-                }
-            }
-            game.setCompteurTours(game.getCompteurTours() + 1);
-            if (game.getCompteurTours() - 1 == game.getNbTours()) {
-                System.out.println("Fin de la partie");
-                return;
-            }
+    public ArrayList<Joueur> getListeJoueur() {
+        return game.getListeJoueur();
+    }
 
-        }
+    public int getCompteurTours() {
+        return game.getCompteurTours();
+    }
+
+    public int getNbTours() {
+        return game.getNbTours();
+    }
+
+    public ArrayList<Case> getPlateau() {
+        return game.getPlateau();
+    }
+
+    public De[] getDes() {
+        return game.getDes();
+    }
+
+    public void setCompteurTours(int compteurTours) {
+        game.setCompteurTours(compteurTours);
+    }
+
+    public void setPlateau() {
+        game.setPlateau();
+    }
+
+    public void createDes() {
+        game.createDes();
     }
 }
--- src/IHM.java
+++ src/IHM.java
@@ -1,7 +1,51 @@
 public class IHM {
     public static void main(String[] args) {
-        CtrlJeu ctrlJeu = new CtrlJeu();
-        ctrlJeu.jouer();
+        int nbTours;
+        int nbJoueurs;
+        System.out.println("Bienvenue dans le jeu Monopoly");
+        System.out.println("Combiens de tours voulez-vous jouer ? (entrez un nombre)");
+        nbTours = Integer.parseInt(System.console().readLine());
+        System.out.println("Combien de joueurs ?");
+        nbJoueurs = Integer.parseInt(System.console().readLine());
+        CtrlJeu ctrlJeu = new CtrlJeu(nbTours, nbJoueurs);
+        ctrlJeu.createGame();
 
+        while(true)
+        {
+            for (Joueur joueur : ctrlJeu.getListeJoueur()) {
+                System.out.println("------------------ TOUR " + ctrlJeu.getCompteurTours() + " DE " + joueur.getPseudo()
+                        + " ---------------------");
+                System.out.println("Appuyez sur entrée pour lancer le dé");
+                System.console().readLine();
+                System.out.println("valeur des dé : " + ctrlJeu.getDes()[0].getValue() + " et " + ctrlJeu.getDes()[1].getValue());
+
+                int deValue = ctrlJeu.getDes()[0].getValue() + ctrlJeu.getDes()[1].getValue();
+                System.out.println(joueur.getPseudo() + " a fait " + deValue);
+                joueur.deplacement(deValue);
+                Case caseCourante = ctrlJeu.getPlateau().get(joueur.getPosition());
+                System.out.println(joueur.getPseudo() + " est sur la case " + caseCourante + "\n");
+
+                // Si le joueur fait un double, il rejoue
+                // To Do: gérer le cas où le joueur va en prison s'il fait 3 doubles de suite
+                while (ctrlJeu.getDes()[0].getValue() == ctrlJeu.getDes()[1].getValue()) {
+                    System.out.println("Vous avez fait un double !");
+                    System.out.println("Vous pouvez rejouer");
+                    System.console().readLine();
+                    System.out.println("valeur des dé : " + ctrlJeu.getDes()[0].getValue() + " et " + ctrlJeu.getDes()[1].getValue());
+                    deValue = ctrlJeu.getDes()[0].getValue() + ctrlJeu.getDes()[1].getValue();
+                    System.out.println(joueur.getPseudo() + " a fait " + deValue);
+                    joueur.deplacement(deValue);
+                    caseCourante = ctrlJeu.getPlateau().get(joueur.getPosition());
+                    System.out.println(joueur.getPseudo() + " est sur la case " + caseCourante + "\n");
+                }
+            }
+            ctrlJeu.setCompteurTours(ctrlJeu.getCompteurTours() + 1);
+            if (ctrlJeu.getCompteurTours() - 1 == ctrlJeu.getNbTours()) {
+                System.out.println("Fin de la partie");
+                return;
+            }
+
+        }
     }
 }
+
