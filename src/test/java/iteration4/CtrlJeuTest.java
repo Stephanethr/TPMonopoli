5c5f6777add2ddbf28f69d277deb267abaacf6af
Stephanethr <stethiry9@gmail.com>
2024-04-03 20:52:53 +0200

    new classe test pour CtrlJeu

---
+++ src/test/java/iteration4/CtrlJeuTest.java
@@ -0,0 +1,64 @@
+package iteration4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CtrlJeuTest {
+
+    @Test
+    void getListeJoueur() {
+    }
+
+    @Test
+    void afficherProprietes() {
+    }
+
+    @Test
+    void getPseudo() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void getArgent() {
+    }
+
+    @Test
+    void getEnPrison() {
+    }
+
+    @Test
+    void getNbToursPrison() {
+    }
+
+    @Test
+    void getCaisseCommunaute() {
+    }
+
+    @Test
+    void piocherCarteChance() {
+    }
+
+    @Test
+    void piocherCarteCaisseCommunaute() {
+    }
+
+    @Test
+    void getNomCase() {
+    }
+
+    @Test
+    void getCaseCourante() {
+    }
+
+    @Test
+    void canReleaseFromJail() {
+    }
+
+    @Test
+    void isDouble() {
+    }
+}
